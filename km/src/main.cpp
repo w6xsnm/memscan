a0efbd4ec08cb59e8b59dacd3fdf5edc2402165a
w6xsnm <w6xsnm@mail.ru>
2025-05-02 22:34:48 +0300

    Simple ioctl driver

--- km/src/main.cpp
+++ km/src/main.cpp
@@ -1,5 +1,15 @@
 #include <ntifs.h>
 
+//undocumented windows internal functions (exported by ntoskrnl)
+extern "C" {
+	NTKERNELAPI NTSTATUS IoCreateDriver(PUNICODE_STRING DriverName,
+										PDRIVER_INITIALIZE InitializaionFunction);
+	NTKERNELAPI NTSTATUS MmCopyVirtualMemory(PEPROCESS SourceProcess, PVOID SourceAddress,
+											 PEPROCESS TargetProcess, PVOID TargetAddress,
+											 SIZE_T BufferSize, KPROCESSOR_MODE PreviousMode,
+											 PSIZE_T ReturnSize);
+}
+
 /**
  * @brief Print debug message from km
  * @param text Debug message
@@ -12,7 +22,176 @@ void DebugPrint(PCSTR text) {
 	KdPrintEx((DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, text));
 }
 
+namespace driver {
+	namespace codes {
+		// Setup the driver
+		constexpr ULONG attach = 
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x696, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+
+		// Read process memory
+		constexpr ULONG read =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x697, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+		
+		// Write process memory
+		constexpr ULONG write =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x698, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+	} //namespace codes
+
+	// Shared between um & km
+	struct Request {
+		HANDLE ProcessId;
+
+		PVOID pTarget;
+		PVOID pBuffer;
+
+		SIZE_T BufferSize;
+		SIZE_T ReturnSize;
+	};
+
+	//IRP major functions
+
+	/*NTSTATUS unsupported(PDEVICE_OBJECT DeviceObject, PIRP irp) {
+		UNREFERENCED_PARAMETER(DeviceObject);
+
+		irp->IoStatus.Status = STATUS_NOT_SUPPORTED;
+
+		IoCompleteRequest(irp, IO_NO_INCREMENT);
+
+		return irp->IoStatus.Status;
+	}*/
+
+	NTSTATUS create(PDEVICE_OBJECT DeviceObject, PIRP irp) {
+		UNREFERENCED_PARAMETER(DeviceObject);
+
+		IoCompleteRequest(irp, IO_NO_INCREMENT);
+
+		return irp->IoStatus.Status;
+	}
+
+	NTSTATUS close(PDEVICE_OBJECT DeviceObject, PIRP irp) {
+		UNREFERENCED_PARAMETER(DeviceObject);
+
+		IoCompleteRequest(irp, IO_NO_INCREMENT);
+
+		return irp->IoStatus.Status;
+	}
+
+	//TODO
+	NTSTATUS device_control(PDEVICE_OBJECT DeviceObject, PIRP irp) {
+		UNREFERENCED_PARAMETER(DeviceObject);
+
+		DebugPrint("[+] Device control called.\n");
+
+		NTSTATUS status = STATUS_UNSUCCESSFUL;
+
+
+		// used to determine ctl code
+		PIO_STACK_LOCATION stack_irp = IoGetCurrentIrpStackLocation(irp);
+
+		// access the request object that um sents
+		auto request = reinterpret_cast<Request*>(irp->AssociatedIrp.SystemBuffer);
+
+		if (stack_irp == nullptr or request == nullptr) {
+			IoCompleteRequest(irp, IO_NO_INCREMENT);
+			return status;
+		}
+
+		// process we want access to (rw)
+		static PEPROCESS TargetProcess = nullptr;
+
+		const ULONG control_code = stack_irp->Parameters.DeviceIoControl.IoControlCode;
+		switch (control_code) {
+			case codes::attach:
+				status = PsLookupProcessByProcessId(request->ProcessId, &TargetProcess);
+				break;
+
+			case codes::read:
+				if (TargetProcess != nullptr) {
+					status = MmCopyVirtualMemory(TargetProcess, request->pTarget,
+												 PsGetCurrentProcess(), request->pBuffer,
+												 request->BufferSize, KernelMode, &request->ReturnSize);
+				}
+				break;
+
+			case codes::write:
+				if (TargetProcess != nullptr) {
+					status = MmCopyVirtualMemory(PsGetCurrentProcess(), request->pBuffer,
+												 TargetProcess, request->pTarget, request->BufferSize, 
+												 KernelMode, &request->ReturnSize);
+				}
+				break;
+
+			default:
+				break;
+		}
+
+		irp->IoStatus.Status = status;
+		irp->IoStatus.Information = sizeof(Request);
+
+		IoCompleteRequest(irp, IO_NO_INCREMENT);
+
+		return irp->IoStatus.Status;
+	}
+
+} // namespace driver
+
+/**
+ * @brief Initialize driver
+ * @param DriverObject Driver object
+ * @param RegistryPath Path to registry key with driver parameters. Never used.
+ * @return 
+ */
+NTSTATUS DriverMain(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	//create device
+	UNICODE_STRING device_name = {};
+	RtlInitUnicodeString(&device_name, L"\\Device\\MemscanDriver");
+
+	PDEVICE_OBJECT DeviceObject;
+	NTSTATUS status = IoCreateDevice(DriverObject, 0, &device_name, FILE_DEVICE_UNKNOWN,
+									 FILE_DEVICE_SECURE_OPEN, FALSE, &DeviceObject);
+
+	if (status != STATUS_SUCCESS) {
+		DebugPrint("[-] Failed to create driver device.\n");
+	}
+	DebugPrint("[+] Driver device successfully created.\n");
+
+	// establish symbolic link
+	UNICODE_STRING symbolic_link = {};
+	RtlInitUnicodeString(&symbolic_link, L"\\DosDevices\\MemscanDriver");
+
+	status = IoCreateSymbolicLink(&symbolic_link, &device_name);
+	if (status != STATUS_SUCCESS) {
+		DebugPrint("[-] Failed to establish symbolic link.\n");
+	}
+	DebugPrint("[+] Symbolic link was established successfully.\n");
+
+	// allow to send small data between um&km
+	SetFlag(DeviceObject->Flags, DO_BUFFERED_IO);
+
+	// set the driver handlers to our functions with our logic
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = driver::create;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = driver::close;
+	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = driver::device_control;
+
+	// end of initialization
+	ClearFlag(DeviceObject->Flags, DO_DEVICE_INITIALIZING);
+
+	DebugPrint("[+] Driver initialized successfully.\n");
+
+	return status;
+}
+
+/**
+ * @brief Trunc function for KdMapper
+ * @return 
+ */
 NTSTATUS DriverEntry() {
-	DebugPrint("[+] Message from kernel!");
-	return STATUS_SUCCESS;
+	DebugPrint("[+] Message from the driver!\n");
+
+	UNICODE_STRING driver_name = {};
+	RtlInitUnicodeString(&driver_name, L"\\Driver\\MemscanDriver");
+
+	return IoCreateDriver(&driver_name, &DriverMain);
 }
--- um/src/main.cpp
+++ um/src/main.cpp
@@ -1,7 +1,166 @@
 #include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+
+/**
+ * @brief Gets ID of a given running process
+ * @param process_name Name of a running process
+ * @return Process ID of a given running process
+ */
+static DWORD GetProcessId(const wchar_t* process_name) {
+	DWORD process_id = 0;
+
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	if (snapshot == INVALID_HANDLE_VALUE) {
+		return process_id;
+	}
+
+	PROCESSENTRY32W entry = {};
+	entry.dwSize = sizeof(decltype(entry));
+
+	if (Process32FirstW(snapshot, &entry) == TRUE) {
+		if (_wcsicmp(process_name, entry.szExeFile) == 0) {
+			process_id = entry.th32ProcessID;
+		}
+		else {
+			while (Process32NextW(snapshot, &entry) == TRUE) {
+				if (_wcsicmp(process_name, entry.szExeFile) == 0) {
+					process_id = entry.th32ProcessID;
+				}
+			}
+		}
+	}
+
+	CloseHandle(snapshot);
+
+	return process_id;
+}
+
+/**
+ * @brief Gets an address of a given module in given process
+ * @param pid Process ID of a process to find module in
+ * @param module_name Name of a module to find in process memory
+ * @return Pointer to a module base or nullptr
+ */
+static std::uintptr_t GetModuleBase(const DWORD pid, const wchar_t* module_name) {
+	std::uintptr_t module_base = 0;
+	
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid);
+	if (snapshot == INVALID_HANDLE_VALUE) {
+		return module_base;
+	}
+
+	MODULEENTRY32W entry = {};
+	entry.dwSize = sizeof(decltype(entry));
+
+	if (Module32FirstW(snapshot, &entry) == TRUE) {
+		if (wcsstr(module_name, entry.szModule) != nullptr) {
+			module_base = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
+		}
+		else {
+			while (Module32NextW(snapshot, &entry) == TRUE) {
+				if (wcsstr(module_name, entry.szModule) != nullptr) {
+					module_base = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
+				}
+			}
+		}
+	}
+
+	CloseHandle(snapshot);
+
+	return module_base;
+}
+
+namespace driver {
+	namespace codes {
+		// Setup the driver
+		constexpr ULONG attach =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x696, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+
+		// Read process memory
+		constexpr ULONG read =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x697, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+
+		// Write process memory
+		constexpr ULONG write =
+			CTL_CODE(FILE_DEVICE_UNKNOWN, 0x698, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+	} //namespace codes
+
+	// Shared between um & km
+	struct Request {
+		HANDLE ProcessId;
+
+		PVOID pTarget;
+		PVOID pBuffer;
+
+		SIZE_T BufferSize;
+		SIZE_T ReturnSize;
+	};
+
+	bool AttachToProcess(HANDLE DriverHandle, const DWORD pid) {
+		Request request;
+		request.ProcessId = reinterpret_cast<HANDLE>(pid);
+
+		return DeviceIoControl(DriverHandle, codes::attach, &request,
+							   sizeof(request), &request, sizeof(request),
+							   nullptr, nullptr);
+	}
+
+	template <class T>
+	T ReadMemory(HANDLE DriverHandle, const std::uintptr_t target_address) {
+		T temp = {};
+
+		Request request;
+		request.pTarget = reinterpret_cast<PVOID>(target_address);
+		request.pBuffer = &temp;
+		request.BufferSize = sizeof(T);
+
+		DeviceIoControl(DriverHandle, codes::read, &request,
+			sizeof(request), &request, sizeof(request),
+			nullptr, nullptr);
+
+		return temp;
+	}
+
+	template <class T>
+	void WriteMemory(HANDLE DriverHandle, const std::uintptr_t target_address, const T& value) {
+		Request request;
+		request.pTarget = reinterpret_cast<PVOID>(target_address);
+		request.pBuffer = (PVOID)&value;
+		request.BufferSize = sizeof(T);
+
+		DeviceIoControl(DriverHandle, codes::write, &request,
+			sizeof(request), &request, sizeof(request),
+			nullptr, nullptr);
+	}
+
+} // namespace driver
 
 int main() {
-	std::cout << "Hello" << std::endl;
+	const DWORD pid = GetProcessId(L"notepad.exe");
+
+	if (pid == 0) {
+		std::cout << "[-] Failed to get process.\n";
+		std::cin.get();
+		return 1;
+	}
+
+	const HANDLE driver = CreateFile(L"\\\\.\\MemscanDriver", GENERIC_READ, 0, nullptr, 
+									 OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+
+	if (driver == INVALID_HANDLE_VALUE) {
+		std::cout << "[-] Failed to create driver handle.\n";
+		std::cin.get();
+		return 1;
+	}
+
+	if (driver::AttachToProcess(driver, pid) == true) {
+		std::cout << "[+] Attachment success.\n";
+	}
+
+	CloseHandle(driver);
+
+	std::cin.get();
 
 	return 0;
 }
